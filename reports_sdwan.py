e1fb8fd10918ece3f2a05a5da29cb93a6dff3e9f
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-02-25 20:22:00 +0800

    moved generateSDWANReport() to another area of the file

--- reports_sdwan.py
+++ reports_sdwan.py
@@ -90,128 +90,6 @@ def generateReport(csvfile, querylist, headers):
     write_to_csv(csvfile, querylist, headers)
 
 
-def generateSDWANReport(zipFileName, startDate, endDate, emailSubject, emailTo):
-
-    printAndLogMessage("Processing [" + emailSubject + "] ...")
-
-    dbConnect()
-    csvFiles.clear()
-
-    columns = [
-        'OrderCode',
-        'NetworkProductCode',
-        'GroupID',
-        'CRD',
-        'TakenDate',
-        'OrderType',
-        'ServiceNumber',
-        'ProjectCode',
-        'CustomerName',
-        'AEndAddress',
-        'FamilyName',
-        'GivenName',
-        'ContactType',
-        'EmailAddress',
-        'ParameterName',
-        'ParameterValue'
-    ]
-
-    orderTypes = ', '.join([("'" + item + "'")
-                            for item in ['Provide',
-                                         'Change']])
-
-    contactTypes = ', '.join([("'" + item + "'")
-                              for item in ['AM',
-                                           'SDE',
-                                           'Project Manager',
-                                           'A-end-Cust']])
-
-    productCodes = ', '.join([("'" + item + "'")
-                              for item in ['SDW0002',
-                                           'SDW0024',
-                                           'SDW0025',
-                                           'SDW0026']])
-
-    parameters = ', '.join([("'" + item + "'")
-                            for item in ['CircuitRef1',
-                                         'CircuitRef2',
-                                         'CircuitRef3',
-                                         'CircuitRef4',
-                                         'CustCircuitTy1',
-                                         'CustCircuitTy2',
-                                         'CustCircuitTy3',
-                                         'CustCircuitTy4',
-                                         'MainEquipModel',
-                                         'OriginCtry',
-                                         'OriginCity',
-                                         'EquipmentVendorPONo',
-                                         'EquipmentVendor',
-                                         'InstallationPartnerPONo',
-                                         'InstallationPartner',
-                                         'SIInstallPartner',
-                                         'SIMaintPartner',
-                                         'CSDWSIInstall',
-                                         'CSDWSIMaint',
-                                         'MainSLA']])
-
-    sqlquery = ("""
-                    SELECT
-                        DISTINCT ORD.order_code AS OrderCode,
-                        PRD.network_product_code AS NetworkProductCode,
-                        PER.role AS GroupID,
-                        ORD.current_crd AS CRD,
-                        ORD.taken_date AS TakenDate,
-                        ORD.order_type AS OrderType,
-                        ORD.service_number AS ServiceNumber,
-                        PRJ.project_code AS ProjectCode,
-                        CUS.name AS CustomerName,
-                        SITE.location AS AEndAddress,
-                        CON.given_name AS FamilyName,
-                        CON.family_name AS GivenName,
-                        CON.contact_type AS ContactType,
-                        CON.email_address AS EmailAddress,
-                        PAR.parameter_name AS ParameterName,
-                        PAR.parameter_name AS ParameterValue
-                    FROM
-                        RestInterface_order ORD
-                        JOIN RestInterface_activity ACT ON ORD.id = ACT.order_id
-                        LEFT JOIN RestInterface_person PER ON ACT.person_id = PER.id
-                        LEFT JOIN RestInterface_project PRJ ON ORD.project_id = PRJ.id
-                        LEFT JOIN RestInterface_customer CUS ON ORD.customer_id = CUS.id
-                        LEFT JOIN RestInterface_site SITE ON ORD.site_id = SITE.id
-                        LEFT JOIN RestInterface_contactdetails CON ON ORD.id = CON.order_id
-                        AND CON.contact_type IN ({})
-                        LEFT JOIN RestInterface_npp NPP ON ORD.id = NPP.order_id
-                        AND NPP.level = 'Mainline'
-                        AND NPP.status <> 'Cancel'
-                        LEFT JOIN RestInterface_parameter PAR ON NPP.id = PAR.npp_id
-                        AND PAR.parameter_name IN ({})
-                        LEFT JOIN RestInterface_product PRD ON NPP.product_id = PRD.id
-                    WHERE
-                        ORD.order_type IN ({})
-                        AND PRD.network_product_code IN ({})
-                        AND ORD.taken_date BETWEEN '{}'
-                        AND '{}'
-                    ORDER BY
-                        OrderCode,
-                        ContactType,
-                        ParameterName;
-                """).format(contactTypes, parameters, orderTypes, productCodes, startDate, endDate)
-
-    csvFile = ("{}_{}.csv").format('GSP_SDN_TM_GSDT_TM', getCurrentDateTime())
-    outputList = dbQueryToList(sqlquery)
-    generateReport(csvFile, outputList, columns)
-
-    dbDisconnect()
-
-    if csvFiles:
-        zipFile = ("{}_{}.zip").format(zipFileName, getCurrentDateTime())
-        zip_file(csvFiles, zipFile, reportsFolderPath)
-        sendEmail(setEmailSubject(emailSubject), zipFile, emailTo)
-
-    printAndLogMessage("Processing [" + emailSubject + "] complete")
-
-
 def zip_csvFile(csvFiles, zipfile):
     with ZipFile(zipfile, 'w') as zipObj:
         for csv in csvFiles:
@@ -370,6 +248,128 @@ def printAndLogError(error):
 
 def getCurrentDateTime():
     return datetime.now().strftime("%d%m%y_%H%M")
+
+
+def generateSDWANReport(zipFileName, startDate, endDate, emailSubject, emailTo):
+
+    printAndLogMessage("Processing [" + emailSubject + "] ...")
+
+    dbConnect()
+    csvFiles.clear()
+
+    columns = [
+        'OrderCode',
+        'NetworkProductCode',
+        'GroupID',
+        'CRD',
+        'TakenDate',
+        'OrderType',
+        'ServiceNumber',
+        'ProjectCode',
+        'CustomerName',
+        'AEndAddress',
+        'FamilyName',
+        'GivenName',
+        'ContactType',
+        'EmailAddress',
+        'ParameterName',
+        'ParameterValue'
+    ]
+
+    orderTypes = ', '.join([("'" + item + "'")
+                            for item in ['Provide',
+                                         'Change']])
+
+    contactTypes = ', '.join([("'" + item + "'")
+                              for item in ['AM',
+                                           'SDE',
+                                           'Project Manager',
+                                           'A-end-Cust']])
+
+    productCodes = ', '.join([("'" + item + "'")
+                              for item in ['SDW0002',
+                                           'SDW0024',
+                                           'SDW0025',
+                                           'SDW0026']])
+
+    parameters = ', '.join([("'" + item + "'")
+                            for item in ['CircuitRef1',
+                                         'CircuitRef2',
+                                         'CircuitRef3',
+                                         'CircuitRef4',
+                                         'CustCircuitTy1',
+                                         'CustCircuitTy2',
+                                         'CustCircuitTy3',
+                                         'CustCircuitTy4',
+                                         'MainEquipModel',
+                                         'OriginCtry',
+                                         'OriginCity',
+                                         'EquipmentVendorPONo',
+                                         'EquipmentVendor',
+                                         'InstallationPartnerPONo',
+                                         'InstallationPartner',
+                                         'SIInstallPartner',
+                                         'SIMaintPartner',
+                                         'CSDWSIInstall',
+                                         'CSDWSIMaint',
+                                         'MainSLA']])
+
+    sqlquery = ("""
+                    SELECT
+                        DISTINCT ORD.order_code AS OrderCode,
+                        PRD.network_product_code AS NetworkProductCode,
+                        PER.role AS GroupID,
+                        ORD.current_crd AS CRD,
+                        ORD.taken_date AS TakenDate,
+                        ORD.order_type AS OrderType,
+                        ORD.service_number AS ServiceNumber,
+                        PRJ.project_code AS ProjectCode,
+                        CUS.name AS CustomerName,
+                        SITE.location AS AEndAddress,
+                        CON.given_name AS FamilyName,
+                        CON.family_name AS GivenName,
+                        CON.contact_type AS ContactType,
+                        CON.email_address AS EmailAddress,
+                        PAR.parameter_name AS ParameterName,
+                        PAR.parameter_name AS ParameterValue
+                    FROM
+                        RestInterface_order ORD
+                        JOIN RestInterface_activity ACT ON ORD.id = ACT.order_id
+                        LEFT JOIN RestInterface_person PER ON ACT.person_id = PER.id
+                        LEFT JOIN RestInterface_project PRJ ON ORD.project_id = PRJ.id
+                        LEFT JOIN RestInterface_customer CUS ON ORD.customer_id = CUS.id
+                        LEFT JOIN RestInterface_site SITE ON ORD.site_id = SITE.id
+                        LEFT JOIN RestInterface_contactdetails CON ON ORD.id = CON.order_id
+                        AND CON.contact_type IN ({})
+                        LEFT JOIN RestInterface_npp NPP ON ORD.id = NPP.order_id
+                        AND NPP.level = 'Mainline'
+                        AND NPP.status <> 'Cancel'
+                        LEFT JOIN RestInterface_parameter PAR ON NPP.id = PAR.npp_id
+                        AND PAR.parameter_name IN ({})
+                        LEFT JOIN RestInterface_product PRD ON NPP.product_id = PRD.id
+                    WHERE
+                        ORD.order_type IN ({})
+                        AND PRD.network_product_code IN ({})
+                        AND ORD.taken_date BETWEEN '{}'
+                        AND '{}'
+                    ORDER BY
+                        OrderCode,
+                        ContactType,
+                        ParameterName;
+                """).format(contactTypes, parameters, orderTypes, productCodes, startDate, endDate)
+
+    csvFile = ("{}_{}.csv").format('GSP_SDN_TM_GSDT_TM', getCurrentDateTime())
+    outputList = dbQueryToList(sqlquery)
+    generateReport(csvFile, outputList, columns)
+
+    dbDisconnect()
+
+    if csvFiles:
+        zipFile = ("{}_{}.zip").format(zipFileName, getCurrentDateTime())
+        zip_file(csvFiles, zipFile, reportsFolderPath)
+        sendEmail(setEmailSubject(emailSubject), zipFile, emailTo)
+
+    printAndLogMessage("Processing [" + emailSubject + "] complete")
 
 
 def main():
