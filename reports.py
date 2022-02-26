36128aa29e26a9d520bc84410a7f4fe2eff5597d
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-02-26 23:53:24 +0800

    added try/catch in main()

--- reports.py
+++ reports.py
@@ -1390,144 +1390,139 @@ def main():
                        datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
     printAndLogMessage("Running script in " + getPlatform())
 
-    if defaultConfig.getboolean('GenReportManually'):
+    try:
+        if defaultConfig.getboolean('GenReportManually'):
 
-        startDate = defaultConfig['ReportStartDate']
-        endDate = defaultConfig['ReportEndDate']
-        printAndLogMessage("start date: " + str(startDate))
-        printAndLogMessage("end date: " + str(endDate))
-
-        # defaultConfig['UpdateTableauDB'] = 'false'
-        printAndLogMessage('UpdateTableauDB = ' +
-                           str(defaultConfig.getboolean('UpdateTableauDB')))
-
-        generateCPluseIpReport('cplusip_report', startDate,
-                               endDate, '', "CPlusIP Report", '')
-        # generateMegaPopReport('megapop_report', startDate,
-        #                       endDate, '', "MegaPop Report", '')
-        # generateSingnetReport('singnet_report', startDate,
-        #                       endDate, '', "Singnet Report", '')
-        # generateStixReport('stix_report', startDate,
-        #                    endDate, "STIX Report", '')
-        # generateInternetReport('internet_report', startDate,
-        #                        endDate, "Internet Report", '')
-        # generateSDWANReport('sdwan_report', startDate,
-        #                     endDate, "SDWAN Report", '')
-        # generateCPluseIpReportGrp(
-        #     'cplusip_report_grp', startDate, endDate, '', "CPlusIP Report", '')
-        # generateMegaPopReportGrp(
-        #     'megapop_report_grp', startDate, endDate, '', "MegaPop Report", '')
-        # generateSingnetReport(
-        #     'singnet_report_apnic', startDate, endDate, 'gsdt7', "Singnet Report - GSP APNIC", 'teckchye@singtel.com;tao.taskrequest@singtel.com')
-        # generateSingnetReport(
-        #     'singnet_report_connectportal', startDate, endDate, 'gsdt7', "Singnet Report – Connect Portal updating", 'kirti.vaish@singtel.com;sandeep.kumarrajendran@singtel.com')
-
-        printAndLogMessage("END of script - " +
-                           datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
-
-    else:
-
-        #-- START --#
-        # If the day falls on the 1st day of the month
-        # start date = 1st day of the month
-        # end date = last day of the month
-
-        # TEST DATA
-        # today_date = datetime.now().date().replace(day=1)
-        # print("date today: " + str(today_date))
-
-        if today_date.day == 1:
-
-            previousMonth = (today_date.replace(day=1) -
-                             timedelta(days=1)).replace(day=today_date.day)
-            startDate = str(previousMonth)
-            lastDay = calendar.monthrange(
-                previousMonth.year, previousMonth.month)[1]
-            endDate = str(previousMonth.replace(day=lastDay))
+            startDate = defaultConfig['ReportStartDate']
+            endDate = defaultConfig['ReportEndDate']
             printAndLogMessage("start date: " + str(startDate))
             printAndLogMessage("end date: " + str(endDate))
 
+            # defaultConfig['UpdateTableauDB'] = 'false'
             printAndLogMessage('UpdateTableauDB = ' +
                                str(defaultConfig.getboolean('UpdateTableauDB')))
 
             generateCPluseIpReport('cplusip_report', startDate,
                                    endDate, '', "CPlusIP Report", '')
-            generateMegaPopReport('megapop_report', startDate,
-                                  endDate, '', "MegaPop Report", '')
-            generateSingnetReport('singnet_report', startDate,
-                                  endDate, '', "Singnet Report", '')
-            generateStixReport('stix_report', startDate,
-                               endDate, "STIX Report", '')
-            generateInternetReport('internet_report', startDate,
-                                   endDate, "Internet Report", '')
-            generateSDWANReport('sdwan_report', startDate,
-                                endDate, "SDWAN Report", '')
+            # generateMegaPopReport('megapop_report', startDate,
+            #                       endDate, '', "MegaPop Report", '')
+            # generateSingnetReport('singnet_report', startDate,
+            #                       endDate, '', "Singnet Report", '')
+            # generateStixReport('stix_report', startDate,
+            #                    endDate, "STIX Report", '')
+            # generateInternetReport('internet_report', startDate,
+            #                        endDate, "Internet Report", '')
+            # generateSDWANReport('sdwan_report', startDate,
+            #                     endDate, "SDWAN Report", '')
+            # generateCPluseIpReportGrp(
+            #     'cplusip_report_grp', startDate, endDate, '', "CPlusIP Report", '')
+            # generateMegaPopReportGrp(
+            #     'megapop_report_grp', startDate, endDate, '', "MegaPop Report", '')
+            # generateSingnetReport(
+            #     'singnet_report_apnic', startDate, endDate, 'gsdt7', "Singnet Report - GSP APNIC", 'teckchye@singtel.com;tao.taskrequest@singtel.com')
+            # generateSingnetReport(
+            #     'singnet_report_connectportal', startDate, endDate, 'gsdt7', "Singnet Report – Connect Portal updating", 'kirti.vaish@singtel.com;sandeep.kumarrajendran@singtel.com')
 
-            printAndLogMessage("END of script - " +
-                               datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
+        else:
 
-        #-- END --#
+            #-- START --#
+            # If the day falls on the 1st day of the month
+            # start date = 1st day of the month
+            # end date = last day of the month
 
-        #-- START --#
-        # If the day falls on the 26th of the month
-        # start date = 26th day of the previous month
-        # end date = 25th day of the current month
+            # TEST DATA
+            # today_date = datetime.now().date().replace(day=1)
+            # print("date today: " + str(today_date))
 
-        # TEST DATA
-        # today_date = datetime.now().date().replace(day=26)
-        # print("date today: " + str(today_date))
+            if today_date.day == 1:
 
-        if today_date.day == 26:
+                previousMonth = (today_date.replace(day=1) -
+                                 timedelta(days=1)).replace(day=today_date.day)
+                startDate = str(previousMonth)
+                lastDay = calendar.monthrange(
+                    previousMonth.year, previousMonth.month)[1]
+                endDate = str(previousMonth.replace(day=lastDay))
+                printAndLogMessage("start date: " + str(startDate))
+                printAndLogMessage("end date: " + str(endDate))
 
-            previousMonth = (today_date.replace(day=1) -
-                             timedelta(days=1)).replace(day=today_date.day)
-            startDate = str(previousMonth)
-            endDate = str(today_date - timedelta(days=1))
-            printAndLogMessage("start date: " + str(startDate))
-            printAndLogMessage("end date: " + str(endDate))
+                printAndLogMessage('UpdateTableauDB = ' +
+                                   str(defaultConfig.getboolean('UpdateTableauDB')))
 
-            defaultConfig['UpdateTableauDB'] = 'false'
-            printAndLogMessage('UpdateTableauDB = ' +
-                               str(defaultConfig.getboolean('UpdateTableauDB')))
+                generateCPluseIpReport('cplusip_report', startDate,
+                                       endDate, '', "CPlusIP Report", '')
+                generateMegaPopReport('megapop_report', startDate,
+                                      endDate, '', "MegaPop Report", '')
+                generateSingnetReport('singnet_report', startDate,
+                                      endDate, '', "Singnet Report", '')
+                generateStixReport('stix_report', startDate,
+                                   endDate, "STIX Report", '')
+                generateInternetReport('internet_report', startDate,
+                                       endDate, "Internet Report", '')
+                generateSDWANReport('sdwan_report', startDate,
+                                    endDate, "SDWAN Report", '')
 
-            generateCPluseIpReportGrp(
-                'cplusip_report_grp', startDate, endDate, '', "CPlusIP Report", '')
-            generateMegaPopReportGrp(
-                'megapop_report_grp', startDate, endDate, '', "MegaPop Report", '')
+            #-- END --#
 
-            printAndLogMessage("END of script - " +
-                               datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
+            #-- START --#
+            # If the day falls on the 26th of the month
+            # start date = 26th day of the previous month
+            # end date = 25th day of the current month
 
-        #-- END --#
+            # TEST DATA
+            # today_date = datetime.now().date().replace(day=26)
+            # print("date today: " + str(today_date))
 
-        #-- START --#
-        # If the day falls on a Monday
-        # start date = date of the previous Monday (T-7)
-        # end date = date of the previous Sunday (T-1)
+            if today_date.day == 26:
 
-        # TEST DATA
-        # today_date = datetime.now().date().replace(day=7)
-        # print("date today: " + str(today_date))
+                previousMonth = (today_date.replace(day=1) -
+                                 timedelta(days=1)).replace(day=today_date.day)
+                startDate = str(previousMonth)
+                endDate = str(today_date - timedelta(days=1))
+                printAndLogMessage("start date: " + str(startDate))
+                printAndLogMessage("end date: " + str(endDate))
 
-        if today_date.isoweekday() == 1:  # Monday
-            startDate = str(today_date - timedelta(days=7))
-            endDate = str(today_date - timedelta(days=1))
-            printAndLogMessage("start date: " + str(startDate))
-            printAndLogMessage("end date: " + str(endDate))
+                defaultConfig['UpdateTableauDB'] = 'false'
+                printAndLogMessage('UpdateTableauDB = ' +
+                                   str(defaultConfig.getboolean('UpdateTableauDB')))
 
-            defaultConfig['UpdateTableauDB'] = 'false'
-            printAndLogMessage('UpdateTableauDB = ' +
-                               str(defaultConfig.getboolean('UpdateTableauDB')))
+                generateCPluseIpReportGrp(
+                    'cplusip_report_grp', startDate, endDate, '', "CPlusIP Report", '')
+                generateMegaPopReportGrp(
+                    'megapop_report_grp', startDate, endDate, '', "MegaPop Report", '')
 
-            generateSingnetReport(
-                'singnet_report_apnic', startDate, endDate, 'gsdt7', "Singnet Report - GSP APNIC", 'teckchye@singtel.com;tao.taskrequest@singtel.com')
-            generateSingnetReport(
-                'singnet_report_connectportal', startDate, endDate, 'gsdt7', "Singnet Report – Connect Portal updating", 'kirti.vaish@singtel.com;sandeep.kumarrajendran@singtel.com')
+            #-- END --#
 
-        #-- END --#
+            #-- START --#
+            # If the day falls on a Monday
+            # start date = date of the previous Monday (T-7)
+            # end date = date of the previous Sunday (T-1)
 
-        printAndLogMessage("END of script - " +
-                           datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
+            # TEST DATA
+            # today_date = datetime.now().date().replace(day=7)
+            # print("date today: " + str(today_date))
+
+            if today_date.isoweekday() == 1:  # Monday
+                startDate = str(today_date - timedelta(days=7))
+                endDate = str(today_date - timedelta(days=1))
+                printAndLogMessage("start date: " + str(startDate))
+                printAndLogMessage("end date: " + str(endDate))
+
+                defaultConfig['UpdateTableauDB'] = 'false'
+                printAndLogMessage('UpdateTableauDB = ' +
+                                   str(defaultConfig.getboolean('UpdateTableauDB')))
+
+                generateSingnetReport(
+                    'singnet_report_apnic', startDate, endDate, 'gsdt7', "Singnet Report - GSP APNIC", 'teckchye@singtel.com;tao.taskrequest@singtel.com')
+                generateSingnetReport(
+                    'singnet_report_connectportal', startDate, endDate, 'gsdt7', "Singnet Report – Connect Portal updating", 'kirti.vaish@singtel.com;sandeep.kumarrajendran@singtel.com')
+
+            #-- END --#
+
+    except Exception as err:
+        printAndLogError(err)
+
+    printAndLogMessage("END of script - " +
+                       datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
 
 
 if __name__ == '__main__':
