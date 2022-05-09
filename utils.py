1a8e9c8b7e413c35145c89f865576a0e90bce066
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-09 17:08:25 +0800

    moved utils function to new utils.py file

---
+++ utils.py
@@ -0,0 +1,74 @@
+import sys
+import logging
+import os
+import configparser
+from datetime import datetime
+from zipfile import ZipFile
+
+log = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+defaultConfig = config['DEFAULT']
+logsFolderPath = os.path.join(os.getcwd(), "logs")
+
+
+def zip_file(csvFiles, zipfile, folderPath):
+
+    printAndLogMessage("Creating " + zipfile + " for " +
+                       ', '.join(csvFiles) + ' ...')
+    os.chdir(folderPath)
+
+    if getPlatform() == "Linux":
+        os_zip_csvFile(csvFiles, zipfile)
+    elif getPlatform() == "Windows":
+        zip_csvFile(csvFiles, zipfile)
+    else:
+        raise Exception("OS " + os + " not supported.")
+
+    os.chdir('../')
+
+
+def zip_csvFile(csvFiles, zipfile):
+    with ZipFile(zipfile, 'w') as zipObj:
+        for csv in csvFiles:
+            csvfilePath = csv
+            zipObj.write(csvfilePath)
+            os.remove(csvfilePath)
+
+
+def os_zip_csvFile(csvFiles, zipfile):
+    csvfiles = ' '.join(csvFiles)
+    os.system("zip -e %s %s -P %s" %
+              (zipfile, csvfiles, defaultConfig['ZipPassword']))
+    for csv in csvFiles:
+        os.remove(csv)
+
+
+def getPlatform():
+    platforms = {
+        'linux': 'Linux',
+        'linux1': 'Linux',
+        'linux2': 'Linux',
+        'darwin': 'OS X',
+        'win32': 'Windows'
+    }
+
+    if sys.platform not in platforms:
+        return sys.platform
+
+    return platforms[sys.platform]
+
+
+def printAndLogMessage(message):
+    print(message)
+    log.info(message)
+
+
+def printAndLogError(error):
+    print("ERROR: {}".format(error))
+    logging.error(error)
+
+
+def getCurrentDateTime():
+    return datetime.now().strftime("%d%m%y_%H%M")
