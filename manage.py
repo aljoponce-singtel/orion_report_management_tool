46b34ab0a041854be1498e83c33fd37379bea5fe
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-13 23:39:07 +0800

    - import module at runtime - call function at runtime

--- manage.py
+++ manage.py
@@ -1,5 +1,17 @@
 import sys
-sys.path.insert(0, './gsp')
-import main
+import importlib
 
-main.main()
+print(sys.argv)
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
+
+sys.path.insert(0, './' + sys.argv[1])
+
+try:
+    importModule = importlib.import_module(sys.argv[2])
+    func = getattr(importModule, sys.argv[3])
+    func()
+except Exception as e:
+    print(e)
