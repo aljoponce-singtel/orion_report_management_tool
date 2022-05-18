78772fe91ed5b4a5000be045a8569e4e073b70bf
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-18 16:23:35 +0800

    added temp debug switch

--- manage.py
+++ manage.py
@@ -1,17 +1,24 @@
-import sys
-import importlib
+debug = False
 
-print(sys.argv)
-print(sys.argv[0])
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
+if debug:
+    from gsp import main
+    main.main()
 
-sys.path.insert(0, './' + sys.argv[1])
+else:
+    import sys
+    import importlib
 
-try:
-    importModule = importlib.import_module(sys.argv[2])
-    func = getattr(importModule, sys.argv[3])
-    func()
-except Exception as e:
-    print(e)
+    print(sys.argv)
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
+
+    sys.path.insert(0, './' + sys.argv[1])
+
+    try:
+        importModule = importlib.import_module(sys.argv[2])
+        func = getattr(importModule, sys.argv[3])
+        func()
+    except Exception as e:
+        print(e)
