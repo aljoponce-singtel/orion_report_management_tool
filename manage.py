6537a093542d65f5e96d476f993bd8d061547c54
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-25 18:02:44 +0800

    output error to file

--- manage.py
+++ manage.py
@@ -1,24 +1,32 @@
-debug = False
+import sys
+import importlib
+from datetime import datetime
 
-if debug:
-    from scripts.gsp import main
-    main.main()
-
-else:
-    import sys
-    import importlib
-
+try:
+    # All arguments
     print(sys.argv)
+    # This file (manage.py)
     print(sys.argv[0])
+    # Report/script folder
     print(sys.argv[1])
+    # Main file/module
     print(sys.argv[2])
+    # function to call
     print(sys.argv[3])
 
+    # Add script folder path to python system path
     sys.path.insert(0, './scripts/' + sys.argv[1])
+    # Import module
+    importModule = importlib.import_module(sys.argv[2])
+    # Call function from the imported module
+    func = getattr(importModule, sys.argv[3])
+    func()
 
-    try:
-        importModule = importlib.import_module(sys.argv[2])
-        func = getattr(importModule, sys.argv[3])
-        func()
-    except Exception as e:
-        print(e)
+except Exception as error:
+    print(error)
+
+    # Output error to file
+    timeStamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    fileName = '{}.error.{}'.format(__file__, timeStamp)
+    errorFile = open(fileName, 'w')
+    errorFile.write(str(error))
