107b2d72cb0d8abc40de7e9afca4a6fefa7801de
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-25 17:10:59 +0800

    added logging information

--- scripts/EmailClient.py
+++ scripts/EmailClient.py
@@ -1,4 +1,3 @@
-from scripts import utils
 import os
 from datetime import datetime
 import smtplib
@@ -37,49 +36,66 @@ class EmailClient:
         return str
 
     def attachFile(self, attachment):
+        logger.info('Attaching file {} ...'.format(
+            os.path.basename(attachment)))
         self.attachments.append(attachment)
 
     def smtpSend(self):
-       # Turn these into plain/html MIMEText objects
-        # part1 = MIMEText(emailBodyText, "plain")
-        part2 = MIMEText(self.emailBodyHtml, "html")
+        logger.info('Sending email with subject "{}" ...'.format(self.subject))
 
-        message = MIMEMultipart()
+        try:
 
-        for attachment in self.attachments:
-            part = MIMEBase('application', "octet-stream")
-            part.set_payload(open(attachment, "rb").read())
-            encoders.encode_base64(part)
-            part.add_header('Content-Disposition',
-                            'attachment; filename="%s"' % os.path.basename(attachment))
-            message.attach(part)
+            # Turn these into plain/html MIMEText objects
+            # part1 = MIMEText(emailBodyText, "plain")
+            part2 = MIMEText(self.emailBodyHtml, "html")
 
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
-        # message.attach(part1)
-        message.attach(part2)
-        message['Subject'] = self.subject
-        message['From'] = self.emailFrom
-        message['To'] = self.receiverTo
-        message['CC'] = self.receiverCc
-        receiver = self.receiverTo + ";" + self.receiverCc
-        smtpObj = smtplib.SMTP(self.server, self.port)
-        smtpObj.sendmail(self.sender, receiver.split(";"), message.as_string())
-        smtpObj.quit()
+            message = MIMEMultipart()
+
+            for attachment in self.attachments:
+                part = MIMEBase('application', "octet-stream")
+                part.set_payload(open(attachment, "rb").read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition',
+                                'attachment; filename="%s"' % os.path.basename(attachment))
+                message.attach(part)
+
+            # Add HTML/plain-text parts to MIMEMultipart message
+            # The email client will try to render the last part first
+            # message.attach(part1)
+            message.attach(part2)
+            message['Subject'] = self.subject
+            message['From'] = self.emailFrom
+            message['To'] = self.receiverTo
+            message['CC'] = self.receiverCc
+            receiver = self.receiverTo + ";" + self.receiverCc
+            smtpObj = smtplib.SMTP(self.server, self.port)
+            smtpObj.sendmail(self.sender, receiver.split(";"),
+                             message.as_string())
+            smtpObj.quit()
+
+        except Exception as e:
+            logger.error("Failed to send email.")
+            logger.exception(e)
 
     def win32comSend(self):
-        import win32com.client
+        logger.info('Sending email with subject "{}" ...'.format(self.subject))
 
-        outlook = win32com.client.Dispatch('outlook.application')
+        try:
+            import win32com.client
+            outlook = win32com.client.Dispatch('outlook.application')
 
-        mail = outlook.CreateItem(0)
-        mail.To = self.receiverTo
-        mail.CC = self.receiverCc
-        mail.Subject = self.subject
-        mail.HTMLBody = self.emailBodyHtml
-        mail.Body = self.emailBodyText
+            mail = outlook.CreateItem(0)
+            mail.To = self.receiverTo
+            mail.CC = self.receiverCc
+            mail.Subject = self.subject
+            mail.HTMLBody = self.emailBodyHtml
+            mail.Body = self.emailBodyText
 
-        for attachment in self.attachments:
-            mail.Attachments.Add(attachment)
+            for attachment in self.attachments:
+                mail.Attachments.Add(attachment)
 
-        mail.Send()
+            mail.Send()
+
+        except Exception as e:
+            logger.error("Failed to send email.")
+            logger.exception(e)
