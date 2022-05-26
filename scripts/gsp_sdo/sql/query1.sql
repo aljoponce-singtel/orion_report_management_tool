dc8ffd89926378437d7268e8bc6ecbb450b230e7
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-26 19:31:09 +0800

    updated query for dynamic variables

--- scripts/gsp_sdo/sql/query1.sql
+++ scripts/gsp_sdo/sql/query1.sql
@@ -1,18 +1,11 @@
 SELECT
-    DISTINCT report_id,
-    Workorder_no,
-    Product_Code,
-    Group_ID_1,
-    Activity_Name_1,
-    COM_1,
-    Group_ID_2,
-    Activity_Name_2,
-    COM_2,
-    DATE(update_time)
+    DISTINCT Workorder_no
 FROM
     t_GSP_ip_svcs
 WHERE
-    report_id IN ('GSDT7')
-    AND DATE(update_time) = '2022-05-01'
-ORDER BY
-    report_id;
+    report_id IN ('{}')
+    AND Order_Type = 'Provide'
+    AND Product_Code IN (
+        {}
+    )
+    AND DATE(update_time) = '{}';
