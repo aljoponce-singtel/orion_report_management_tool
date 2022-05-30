4e4b3a7ebd52c4a7dc8b028ee5b253c5b3c5933b
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-05-30 17:00:54 +0800

    new constants.py file to store constants

---
+++ scripts/gsp_sdo/constants.py
@@ -0,0 +1,82 @@
+PARAMETERS_TO_SEARCH = [
+    'FTTHNo',
+    'MetroENo',
+    'GigawaveNo'
+]
+
+FINAL_COLUMNS = [
+    'OrderCode',
+    'ServiceNumber',
+    'ProductCode',
+    'CRD',
+    'CustomerName',
+    'OrderCreated',
+    'OrderType',
+    'OrderCodeNew',
+    'CRDNew',
+    'ServiceNoNew',
+    'StepNo_SS',
+    'ActName_SS',
+    'GrpID_SS',
+    'DueDate_SS',
+    'RDY_Date_SS',
+    'EXC_Date_SS',
+    'DLY_Date_SS',
+    'COM_Date_SS',
+    'StepNo_RI',
+    'ActName_RI',
+    'GrpID_RI',
+    'DueDate_RI',
+    'RDY_Date_RI',
+    'EXC_Date_RI',
+    'DLY_Date_RI',
+    'COM_Date_RI',
+    'StepNo_TI',
+    'ActName_TI',
+    'GrpID_TI',
+    'DueDate_TI',
+    'RDY_Date_TI',
+    'EXC_Date_TI',
+    'DLY_Date_TI',
+    'COM_Date_TI',
+    'ProjectManager'
+]
+
+SS_COLUMNS = [
+    'OrderCodeNew',
+    'GrpID_SS',
+    'StepNo_SS',
+    'ActName_SS',
+    'DueDate_SS',
+    'Status_SS',
+    'RDY_Date_SS',
+    'EXC_Date_SS',
+    'DLY_Date_SS',
+    'COM_Date_SS'
+]
+
+RI_COLUMNS = [
+    'OrderCodeNew',
+    'GrpID_RI',
+    'StepNo_RI',
+    'ActName_RI',
+    'DueDate_RI',
+    'Status_RI',
+    'RDY_Date_RI',
+    'EXC_Date_RI',
+    'DLY_Date_RI',
+    'COM_Date_RI'
+]
+
+TI_COLUMNS = [
+    'OrderCodeNew',
+    'GrpID_TI',
+    'StepNo_TI',
+    'ActName_TI',
+    'DueDate_TI',
+    'Status_TI',
+    'RDY_Date_TI',
+    'EXC_Date_TI',
+    'DLY_Date_TI',
+    'COM_Date_TI'
+]
