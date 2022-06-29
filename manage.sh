4bb2481b3ed071612bdebe629dd5279f6218d8ef
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-06-29 13:47:35 +0800

    added production and dev environment options

--- manage.sh
+++ manage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Production
+# source '/app/o2p/ossadmin/python3-env/bin/activate'
+# cd /app/o2p/ossadmin/orion_report_management_tool
+
+# DEV
 source 'c:/Users/p1319639/Development/pythonenv/Scripts/activate'
-cd /app/o2p/ossadmin/orion_report_management_tool
 
 echo "Parsed arguments: $@"
 python manage.py $@
