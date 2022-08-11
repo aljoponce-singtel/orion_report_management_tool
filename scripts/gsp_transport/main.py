a972a393d4b773aa9e9644a9045ad8145ae1e044
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-08-12 00:13:17 +0800

    draft

--- scripts/gsp_transport/main.py
+++ scripts/gsp_transport/main.py
@@ -1,22 +1,27 @@
-import sys
+# import sys
 import os
 import logging
 import configparser
+from datetime import datetime
+from scripts import log
+from scripts import utils
+from scripts.gsp_transport import reports
 
-# Getting the name of the directory where this file is present.
-current = os.path.dirname(os.path.realpath(__file__))
-# Getting the parent directory name where the current directory is present.
-parent = os.path.dirname(current)
-# Adding the parent directory to the sys.path.
-sys.path.append(parent)
-# Import modules as recognized from sys.path.
-import log
-import utils
+# # Getting the name of the directory where this file is present.
+# current = os.path.dirname(os.path.realpath(__file__))
+# # Getting the parent directory name where the current directory is present.
+# parent = os.path.dirname(current)
+# # Adding the parent directory to the sys.path.
+# sys.path.append(parent)
+# # Import modules as recognized from sys.path.
+
+# import log
+# import utils
 
 config = configparser.ConfigParser()
-# configFile = 'scripts/gsp_sdwan/config.ini'
-configFile = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), 'config.ini')
+configFile = 'scripts/gsp_transport/config.ini'
+# configFile = os.path.join(os.path.dirname(
+#     os.path.realpath(__file__)), 'config.ini')
 config.read(configFile)
 log.initialize(configFile)
 defaultConfig = config['DEFAULT']
@@ -24,7 +29,12 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    print(utils.getPlatform())
+    logger.info("==========================================")
+    logger.info("START of script - " +
+                datetime.now().strftime("%a %m/%d/%Y, %H:%M:%S"))
+    logger.info("Running script in " + utils.getPlatform())
+
+    today_date = datetime.now().date()
 
 
 if __name__ == '__main__':
--- scripts/gsp_transport/reports.py
+++ scripts/gsp_transport/reports.py
@@ -3,20 +3,23 @@ import sys
 import logging.config
 import logging
 import pandas as pd
+from scripts import utils
+from scripts.DBConnection import DBConnection
+from scripts.EmailClient import EmailClient
 
-# getting the name of the directory
-# where this file is present.
-current = os.path.dirname(os.path.realpath(__file__))
-# Getting the parent directory name
-# where the current directory is present.
-parent = os.path.dirname(current)
-# adding the parent directory to
-# the sys.path.
-sys.path.append(parent)
+# # getting the name of the directory
+# # where this file is present.
+# current = os.path.dirname(os.path.realpath(__file__))
+# # Getting the parent directory name
+# # where the current directory is present.
+# parent = os.path.dirname(current)
+# # adding the parent directory to
+# # the sys.path.
+# sys.path.append(parent)
 
-import utils
-from DBConnection import DBConnection
-from EmailClient import EmailClient
+# import utils
+# from DBConnection import DBConnection
+# from EmailClient import EmailClient
 
 logger = logging.getLogger(__name__)
 defaultConfig = None
