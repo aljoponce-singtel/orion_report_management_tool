a43aee551b553c2e2fe4fd00d598b70f0aa8c935
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-10-24 23:53:20 +0800

    log reports folder path and log file path
    new functions:
    - createCsvFromDataframe
    - sendEmail

--- scripts/OrionReport.py
+++ scripts/OrionReport.py
@@ -43,6 +43,8 @@ class OrionReport(EmailClient):
             scriptFolderPath), "logging.yml")
 
         try:
+            # Setup logging
+            logsFolder = None
 
             with open(loggerConfig, 'r') as stream:
 
@@ -51,9 +53,8 @@ class OrionReport(EmailClient):
                 if os.path.exists(os.path.join(scriptFolderPath, "logging.yml")):
                     logging.config.dictConfig(parsed_yaml)
                 else:
-                    if config.has_option('DEBUG', 'logToFile') == True:
-                        if self.debugConfig.getboolean('logToFile') == False:
-                            parsed_yaml['root']['handlers'] = ['console']
+                    if config.has_option('DEBUG', 'logToFile') == True and self.debugConfig.getboolean('logToFile') == False:
+                        parsed_yaml['root']['handlers'] = ['console']
 
                     if config.has_option('DEFAULT', 'logFile') == False:
                         logsFolder = os.path.join('logs', scriptFolderName)
@@ -65,17 +66,25 @@ class OrionReport(EmailClient):
 
                     logging.config.dictConfig(parsed_yaml)
 
-                    if config.has_option('DEBUG', 'logLevel') == True:
-                        logger.setLevel(self.getLevelNumValue(
-                            self.debugConfig['logLevel']))
+                logFilePath = os.path.realpath(
+                    parsed_yaml['handlers']['timedRotatingFile']['filename'])
+                logger.info(f'Log path: {str(logFilePath)}')
 
+                # Set log level
+                if config.has_option('DEBUG', 'logLevel') == True:
+                    logger.setLevel(self.getLevelNumValue(
+                        self.debugConfig['logLevel']))
+
+            # Setup reports folder
             if config.has_option('DEFAULT', 'reportsFolder') == False:
                 self.reportsFolderPath = os.path.join(
                     'reports', scriptFolderName)
-                utils.createFolder(self.reportsFolderPath)
             else:
                 self.reportsFolderPath = self.defaultConfig['reportsFolder']
-                utils.createFolder(self.reportsFolderPath)
+
+            self.reportsFolderPath = os.path.realpath(self.reportsFolderPath)
+            logger.info(f'Reports folder: {str(self.reportsFolderPath)}')
+            utils.createFolder(self.reportsFolderPath)
 
         except Exception as err:
             logger.exception(err)
@@ -110,3 +119,54 @@ class OrionReport(EmailClient):
             return 50
         else:  # 'NOTSET'
             return 0
+
+    def createCsvFromDataframe(self, df, csvFilePath):
+        logger.info("Generating report " +
+                    os.path.basename(csvFilePath) + " ...")
+        if self.debugConfig.getboolean('createReport') == True:
+            utils.dataframeToCsv(df, csvFilePath)
+
+    def sendEmail(self):
+        # Enable/Disable email
+        if self.debugConfig.getboolean('sendEmail') == True:
+            try:
+                self.receiverTo = self.emailConfig["receiverTo"]
+                self.receiverCc = self.emailConfig["receiverCc"]
+
+                if self.subject == None:
+                    self.subject = self.addTimestamp2("Orion Report")
+
+                if self.emailBodyText == None:
+                    self.emailBodyText = """
+                        Hello,
+
+                        Please see attached ORION report.
+
+
+                        Thanks you and best regards,
+                        Orion Team
+                    """
+
+                if self.emailBodyHtml == None:
+                    self.emailBodyhtml = """\
+                        <html>
+                        <p>Hello,</p>
+                        <p>Please see attached ORION report.</p>
+                        <p>&nbsp;</p>
+                        <p>Thank you and best regards,</p>
+                        <p>Orion Team</p>
+                        </html>
+                        """
+
+                if utils.getPlatform() == 'Windows':
+                    self.win32comSend()
+                else:
+                    self.server = self.emailConfig['server']
+                    self.port = self.emailConfig['port']
+                    self.sender = self.emailConfig['sender']
+                    self.emailFrom = self.emailConfig["from"]
+                    self.smtpSend()
+
+            except Exception as e:
+                logger.error("Failed to send email.")
+                raise Exception(e)
