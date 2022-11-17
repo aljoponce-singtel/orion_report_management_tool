0f5fcff4dabaaa754fd53500a4eb9b16e373b570
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-11-17 17:15:33 +0800

    removed unused comments

--- scripts/reports/gsp_war_room/main.py
+++ scripts/reports/gsp_war_room/main.py
@@ -1,13 +1,5 @@
-# from scripts.reports.gsp_war_room import reports
 from helper import generateWarRoomReport
-import os
-import sys
 import logging
-
-# Add the parent (scripts) directory to sys.path
-# currentDir = os.path.dirname(os.path.realpath(__file__))
-# parentDir = os.path.dirname(currentDir)
-# sys.path.append(parentDir)
 
 logger = logging.getLogger(__name__)
 
