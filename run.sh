a1d311cb36fe18b8f01501fb1c538d2fbc3d5b30
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2022-11-17 18:03:16 +0800

    support running in Windows and Linux

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Production
-# source '/app/o2p/ossadmin/python3-env/bin/activate'
-# cd /app/o2p/ossadmin/orion_report_management_tool
+# Get the OS information
+os=$(uname -a)
 
-# DEV
-source 'c:/Users/p1319639/Development/pythonenv/Scripts/activate'
+# Windows OS
+if echo "$os" | grep -q "MINGW64"; then
+    # DEV
+    source 'c:/Users/p1319639/Development/pythonenv/Scripts/activate'
+# Linux OS
+else
+    # Production
+    source '/app/o2p/ossadmin/python3-env/bin/activate'
+    cd /app/o2p/ossadmin/orion_report_management_tool
+fi
 
-echo "Parsed arguments: $@"
 python run.py $@
