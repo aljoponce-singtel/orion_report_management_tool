5b817844dceca64333d64aa2894345ecc1129d5a
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2023-08-14 11:50:20 +0800

    added two functions: get_monday_date_from_prev_week() get_sunday_date_from_prev_week()

--- scripts/helpers/utils.py
+++ scripts/helpers/utils.py
@@ -165,6 +165,32 @@ def get_end_date_from_prev_month(date):
     return date - timedelta(days=1)
 
 
+def get_monday_date_from_prev_week(date):
+    # Example:
+    # 2023-08-15 = Current date
+    # 2023-08-07 = Monday's date from previous week
+
+    # Calculate the number of days since the last Monday (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+    days_since_last_monday = (date.weekday() - 0) % 7
+    # Calculate the date of the last week's Monday
+    last_week_monday = date - timedelta(days=days_since_last_monday + 7)
+
+    return last_week_monday
+
+
+def get_sunday_date_from_prev_week(date):
+    # Example:
+    # 2023-08-15 = Current date
+    # 2023-08-13 = Sunday's date from previous week
+
+    # Calculate the number of days since the last Monday (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+    days_since_last_monday = (date.weekday() - 0) % 7
+    # Calculate the date of the last week's Sunday
+    last_week_sunday = date - timedelta(days=days_since_last_monday + 1)
+
+    return last_week_sunday
+
+
 def create_folder(folder_path, overwrite=False):
     # Check if the specified path exists
     is_exist = os.path.exists(folder_path)
