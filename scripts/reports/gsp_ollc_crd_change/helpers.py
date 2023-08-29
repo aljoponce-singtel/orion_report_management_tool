e7e9d3806ae64d44e36025fdbe4ff5bbe8a4513b
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2023-08-29 15:33:53 +0800

    updated_report

--- scripts/reports/gsp_ollc_crd_change/helpers.py
+++ scripts/reports/gsp_ollc_crd_change/helpers.py
@@ -12,25 +12,24 @@ from scripts.helpers import utils
 from scripts.orion_report import OrionReport
 
 logger = logging.getLogger(__name__)
-configFile = os.path.join(os.path.dirname(__file__), 'config.ini')
+config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
 
 
 def generate_report():
 
-    report = OrionReport(configFile)
+    report = OrionReport(config_file)
 
-    email_subject = 'GSP OLLC CRD Change Report'
-    filename = 'gsp_ollc_crd_change_report'
-    report_date = datetime.now().date()
+    report.subject = 'GSP OLLC CRD Change Report'
+    report.filename = 'gsp_ollc_crd_change_report'
 
     if report.debug_config.getboolean('generate_manual_report'):
         logger.info('\\* MANUAL RUN *\\')
-        report_date = report.debug_config['report_date']
+        report.report_date = report.debug_config['report_date']
 
     else:
-        report_date = datetime.now().date()
+        report.report_date = datetime.now().date()
 
-    logger.info("report date: " + str(report_date))
+    logger.info("report date: " + str(report.report_date))
     logger.info("Generating gsp ollc crd change report ...")
 
     query = f"""
@@ -54,7 +53,7 @@ def generate_report():
                         FROM
                             o2pprod.Order_CRD_History HIST
                         WHERE
-                            DATE_FORMAT(HIST.updated_at, '%y-%m-%d') < DATE_FORMAT('{report_date}', '%y-%m-%d')
+                            DATE_FORMAT(HIST.updated_at, '%y-%m-%d') < DATE_FORMAT('{report.report_date}', '%y-%m-%d')
                         GROUP BY
                             workorderno
                     ) BBB,
@@ -77,7 +76,7 @@ def generate_report():
                         FROM
                             o2pprod.Order_CRD_History HIST
                         WHERE
-                            DATE_FORMAT(HIST.updated_at, '%y-%m-%d') = DATE_FORMAT('{report_date}', '%y-%m-%d')
+                            DATE_FORMAT(HIST.updated_at, '%y-%m-%d') = DATE_FORMAT('{report.report_date}', '%y-%m-%d')
                     ) CCC,
                     RestInterface_order ORD
                     LEFT JOIN RestInterface_project PRJ ON PRJ.id = ORD.project_id,
@@ -114,10 +113,8 @@ def generate_report():
         # set the final columns for the report
         df_main = df_raw[const.MAIN_COLUMNS]
 
-        # Write to CSV for Warroom Report
-        csv_file = ("{}_{}.csv").format(filename, utils.get_current_datetime())
-        csv_main_file_path = os.path.join(report.reports_folder_path, csv_file)
-        report.create_csv_from_df(df_main, csv_main_file_path)
+        # Export df_main to CSV
+        csv_file = report.create_csv_from_df(df_main)
 
         df_pm = df_raw[df_raw['assignee'] == 'PM']
         df_pm = df_pm[const.MAIN_COLUMNS]
@@ -159,8 +156,8 @@ def generate_report():
 
         report.set_email_body_text(email_body_text)
         report.set_email_body_html(email_body_html)
-        report.set_email_subject(report.add_timestamp(email_subject))
-        report.attach_file_to_email(csv_main_file_path)
+        report.set_email_subject(report.add_timestamp(report.subject))
+        report.attach_file_to_email(csv_file)
         report.send_email()
     else:
         logger.info("No OLLC workorders where their CRD was changed today.")
