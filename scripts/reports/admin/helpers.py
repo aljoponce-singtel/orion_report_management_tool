8a70f0a86c8faf0e32d3820784939ded61ade929
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2023-10-09 17:04:39 +0800

    new check_new_product_codes_to_map()()

--- scripts/reports/admin/helpers.py
+++ scripts/reports/admin/helpers.py
@@ -6,6 +6,7 @@ import logging
 import pandas as pd
 
 # Import local packages
+from os.path import basename
 from scripts.helpers import utils
 from scripts.orion_report import OrionReport
 
@@ -26,7 +27,7 @@ def query_to_file(query_file, filename, report_name='Quick Query'):
         query = file.read()
 
     csv_file = report.query_to_csv(query, add_timestamp=True)
-    report.attach_file(csv_file)
+    report.attach_file_to_email(csv_file)
     report.send_email()
 
 
@@ -40,16 +41,6 @@ def check_disk_usage():
         logger.warn(
             f"The drive at {path_to_check} is {disk_usage_percentage:.2f}% full.")
 
-        # email_body_html = f"""\
-        #     <html>
-        #     <p>Hello,</p>
-        #     <p>The drive at {path_to_check} is {disk_usage_percentage:.2f}% full.</p>
-        #     <p>&nbsp;</p>
-        #     <p>Best regards,</p>
-        #     <p>The Orion Team</p>
-        #     </html>
-        #     """
-
         email_body_html = f"""\
             <html>
             <p>Hello,</p>
@@ -62,7 +53,7 @@ def check_disk_usage():
 
         report.set_email_body_html(email_body_html)
         report.add_email_receiver_to("jiangxu.jiang@singtel.com")
-        report.attach_file(report.log_file_path)
+        report.attach_file_to_email(report.log_file_path)
         report.send_email()
     else:
         logger.info(
@@ -72,54 +63,14 @@ def check_disk_usage():
 def check_new_queueowners():
 
     report = OrionReport(configFile, 'New Queue Owner Updates')
+    query_file = os.path.join(os.path.dirname(
+        __file__), 'sql', "query_new_queueowner.sql")
+    query = None
 
-    query = f"""
-                SELECT
-                    DISTINCT PER.role AS 'group_id',
-                    PER.email AS 'user',
-                    PER.user_id,
-                    (
-                        CASE
-                            WHEN (
-                                PER.email LIKE '%singtel.com'
-                                AND NOT (
-                                    PER.email LIKE '%,%'
-                                    OR PER.email LIKE '%;%'
-                                    OR PER.email LIKE '%\%'
-                                    OR PER.email LIKE '%/%'
-                                ) -- AND PER.email NOT REGEXP '[,;\/\\\\]'
-                            ) THEN 'yes'
-                            ELSE 'no'
-                        END
-                    ) AS is_valid,
-                    (
-                        CASE
-                            WHEN GRP.is_enabled = 1 THEN 'yes'
-                            ELSE 'no'
-                        END
-                    ) AS is_enabled,
-                    USR_ACTUAL.username AS 'actual_user',
-                    USR_ACTUAL.id AS 'actual_user_id',
-                    USR_EXPECTED.username AS 'expected_user',
-                    USR_EXPECTED.id AS 'expected_user_id',
-                    PER.created_at,
-                    PER.updated_at
-                FROM
-                    RestInterface_person PER
-                    JOIN auto_escalation_escalationmatrix MTX ON MTX.person_id = PER.id
-                    JOIN auto_escalation_escalationgroup GRP ON GRP.person_id = PER.id
-                    LEFT JOIN RestInterface_user USR_ACTUAL ON USR_ACTUAL.id = PER.user_id
-                    LEFT JOIN RestInterface_user USR_EXPECTED ON USR_EXPECTED.username = TRIM(PER.email)
-                WHERE
-                    PER.email != ''
-                    AND (
-                        TRIM(PER.email) != USR_ACTUAL.username
-                        OR PER.user_id IS NULL
-                    )
-                ORDER BY
-                    PER.email,
-                    PER.role;
-            """
+    # Open the file in read mode
+    with open(query_file, 'r') as file:
+        # Read the entire contents of the file into a string
+        query = file.read()
 
     df = report.orion_db.query_to_dataframe(query)
 
@@ -189,7 +140,7 @@ def check_new_queueowners():
 
         # Send email
         report.set_email_body_html(email_body_html)
-        report.attach_file(report.log_file_path)
+        report.attach_file_to_email(report.log_file_path)
 
         if df_valid.empty:
             logger.info("NO NEW QUEUE OWNERS ADDED/UPDATED TODAY")
@@ -198,3 +149,51 @@ def check_new_queueowners():
 
     else:
         logger.info("NO NEW QUEUE OWNERS ADDED/UPDATED TODAY")
+
+
+def check_new_product_codes_to_map():
+
+    report = OrionReport(configFile, 'New Product Codes To Map')
+    query_file = os.path.join(os.path.dirname(
+        __file__), 'sql', "query_producttype.sql")
+    query = None
+    query_update_file = os.path.join(os.path.dirname(
+        __file__), 'sql', "update_producttype.sql")
+
+    # Open the file in read mode
+    with open(query_file, 'r') as file:
+        # Read the entire contents of the file into a string
+        query = file.read()
+
+    df = report.orion_db.query_to_dataframe(query)
+
+    if not df.empty:
+        # Remove None values
+        df.fillna('', inplace=True)
+        # Reset and change starting index from 0 to 1 for proper table presentation
+        df = df.reset_index(drop=True)
+        df.index += 1
+
+        query_file_txt = report.attach_file_to_email(
+            query_file, append_file_ext='txt')
+        query_update_file_txt = report.attach_file_to_email(
+            query_update_file, append_file_ext='txt')
+
+        email_body_html = f"""\
+            <html>
+            <p>Hello,</p>
+            <p>Please see below the list of new product codes to be mapped to the product types table.</p>
+            <p>{df.to_html()}</p>
+            <p>See attached file {basename(query_file_txt)} for the query.</p>
+            <p>See attached file {basename(query_update_file_txt)} to map the product code.</p>
+            <p>&nbsp;</p>
+            <p>Best regards,</p>
+            <p>The Orion Team</p>
+            </html>
+            """
+
+        report.set_email_body_html(email_body_html)
+        report.send_email()
+
+    else:
+        logger.info("NO NEW PRODUCT CODES TO MAP TODAY")
--- scripts/reports/admin/main.py
+++ scripts/reports/admin/main.py
@@ -2,7 +2,7 @@
 import logging
 
 # Import local packages
-from helpers import check_disk_usage, check_new_queueowners, query_to_file
+from helpers import check_disk_usage, check_new_queueowners, query_to_file, check_new_product_codes_to_map
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +13,7 @@ def main():
         check_disk_usage()
         check_new_queueowners()
         # query_to_file('query_aop.sql', 'gsp_aop', 'GSP AOP Report')
+        # check_new_product_codes_to_map()
 
     except Exception as err:
         logger.exception(err)
