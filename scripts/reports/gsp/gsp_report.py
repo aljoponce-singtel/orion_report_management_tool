23464579ea16cee0c3d94ea7affa99c56ed9f74d
aljoponce-singtel <96461798+aljoponce-singtel@users.noreply.github.com>
2023-10-12 17:20:05 +0800

    updated generate_report_two_group() to check if df_report is empty

--- scripts/reports/gsp/gsp_report.py
+++ scripts/reports/gsp/gsp_report.py
@@ -198,99 +198,102 @@ class GspReport(OrionReport):
 
         # New dataframe for the final report
         df_report = pd.DataFrame(columns=const.MAIN_COLUMNS_DOUBLE)
-        # Get the list of unique workorders
-        unique_orders = df_raw['Workorder no'].unique()
 
-        # Iterate through the records of each uniqe workorders
-        for order in unique_orders:
-            # Initialize group 1 and 2 dataframe and dictionary
-            df_top_group_1 = pd.DataFrame(columns=const.RAW_COLUMNS)
-            df_top_group_2 = pd.DataFrame(columns=const.RAW_COLUMNS)
-            group_1_dict = {
-                col: None if col not in const.DATE_COLUMNS else pd.NaT for col in const.RAW_COLUMNS}
-            group_2_dict = {
-                col: None if col not in const.DATE_COLUMNS else pd.NaT for col in const.RAW_COLUMNS}
-            # Get the records of an order
-            df_order: pd.DataFrame
-            df_order = df_raw[df_raw['Workorder no'] == order]
+        # Check if the dataframe is not empty
+        if not df_report.empty:
+            # Get the list of unique workorders
+            unique_orders = df_raw['Workorder no'].unique()
 
-            if only_group_id == False:
-                # Get the group_id and activity records that matches group 1
-                df_group_1 = df_order[df_order['Group ID'].isin(
-                    self.first_groupid_list) & df_order['Activity Name'].isin(self.first_activity_list)]
-                # Get the group_id and activity records that matches group 2
-                df_group_2 = df_order[df_order['Group ID'].isin(
-                    self.second_groupid_list) & df_order['Activity Name'].isin(self.second_activity_list)]
-            else:
-                # Get the group_id records that matches group 1
-                df_group_1 = df_order[df_order['Group ID'].isin(
-                    self.first_groupid_list)]
-                # Get the group_id records that matches group 2
-                df_group_2 = df_order[df_order['Group ID'].isin(
-                    self.second_groupid_list)]
+            # Iterate through the records of each uniqe workorders
+            for order in unique_orders:
+                # Initialize group 1 and 2 dataframe and dictionary
+                df_top_group_1 = pd.DataFrame(columns=const.RAW_COLUMNS)
+                df_top_group_2 = pd.DataFrame(columns=const.RAW_COLUMNS)
+                group_1_dict = {
+                    col: None if col not in const.DATE_COLUMNS else pd.NaT for col in const.RAW_COLUMNS}
+                group_2_dict = {
+                    col: None if col not in const.DATE_COLUMNS else pd.NaT for col in const.RAW_COLUMNS}
+                # Get the records of an order
+                df_order: pd.DataFrame
+                df_order = df_raw[df_raw['Workorder no'] == order]
 
-            # Check if group 1 is not empty
-            if not df_group_1.empty:
-                # Check if group 1 has more than 1 records
-                if len(df_group_1) > 1:
-                    # Get the top records based on priority and sequence
-                    df_top_group_1 = self.__select_top_queue(
-                        df_group_1, 'Activity Name', self.first_activity_list)
+                if only_group_id == False:
+                    # Get the group_id and activity records that matches group 1
+                    df_group_1 = df_order[df_order['Group ID'].isin(
+                        self.first_groupid_list) & df_order['Activity Name'].isin(self.first_activity_list)]
+                    # Get the group_id and activity records that matches group 2
+                    df_group_2 = df_order[df_order['Group ID'].isin(
+                        self.second_groupid_list) & df_order['Activity Name'].isin(self.second_activity_list)]
                 else:
-                    df_top_group_1 = df_group_1
-            # Check if group 2 is not empty
-            if not df_group_2.empty:
-                # Check if group 2 has more than 1 records
-                if len(df_group_2) > 1:
-                    # Get the top records based on priority and sequence
-                    df_top_group_2 = self.__select_top_queue(
-                        df_group_2, 'Activity Name', self.second_activity_list)
-                else:
-                    df_top_group_2 = df_group_2
+                    # Get the group_id records that matches group 1
+                    df_group_1 = df_order[df_order['Group ID'].isin(
+                        self.first_groupid_list)]
+                    # Get the group_id records that matches group 2
+                    df_group_2 = df_order[df_order['Group ID'].isin(
+                        self.second_groupid_list)]
 
-            # Convert the dataframe to a dictionary
-            order_dict = df_order.head(1).to_dict(orient='records')[0]
-            if not df_group_1.empty:
-                group_1_dict = df_top_group_1.to_dict(orient='records')[0]
-            if not df_group_2.empty:
-                group_2_dict = df_top_group_2.to_dict(orient='records')[0]
+                # Check if group 1 is not empty
+                if not df_group_1.empty:
+                    # Check if group 1 has more than 1 records
+                    if len(df_group_1) > 1:
+                        # Get the top records based on priority and sequence
+                        df_top_group_1 = self.__select_top_queue(
+                            df_group_1, 'Activity Name', self.first_activity_list)
+                    else:
+                        df_top_group_1 = df_group_1
+                # Check if group 2 is not empty
+                if not df_group_2.empty:
+                    # Check if group 2 has more than 1 records
+                    if len(df_group_2) > 1:
+                        # Get the top records based on priority and sequence
+                        df_top_group_2 = self.__select_top_queue(
+                            df_group_2, 'Activity Name', self.second_activity_list)
+                    else:
+                        df_top_group_2 = df_group_2
 
-            report_data = [
-                order_dict['Workorder no'],
-                order_dict['Service No'],
-                order_dict['Product Code'],
-                order_dict['Product Description'],
-                order_dict['Customer Name'],
-                order_dict['Order Type'],
-                order_dict['CRD'],
-                order_dict['Order Taken Date'],
-                group_1_dict['Group ID'],
-                group_1_dict['Activity Name'],
-                group_1_dict['DUE'],
-                # set RDY date to COM date if RDY date is empty
-                group_1_dict['RDY'] if not pd.isna(
-                    group_1_dict['RDY']) else group_1_dict['COM'],
-                # set EXC date to COM date if EXC date is empty
-                group_1_dict['EXC'] if not pd.isna(
-                    group_1_dict['EXC']) else group_1_dict['COM'],
-                group_1_dict['DLY'],
-                group_1_dict['COM'],
-                group_2_dict['Group ID'],
-                group_2_dict['Activity Name'],
-                group_2_dict['DUE'],
-                # set RDY date to COM date if RDY date is empty
-                group_2_dict['RDY'] if not pd.isna(
-                    group_2_dict['RDY']) else group_2_dict['COM'],
-                # set EXC date to COM date if EXC date is empty
-                group_2_dict['EXC'] if not pd.isna(
-                    group_2_dict['EXC']) else group_2_dict['COM'],
-                group_2_dict['DLY'],
-                group_2_dict['COM']
-            ]
+                # Convert the dataframe to a dictionary
+                order_dict = df_order.head(1).to_dict(orient='records')[0]
+                if not df_group_1.empty:
+                    group_1_dict = df_top_group_1.to_dict(orient='records')[0]
+                if not df_group_2.empty:
+                    group_2_dict = df_top_group_2.to_dict(orient='records')[0]
 
-            df_to_add = pd.DataFrame(
-                data=[report_data], columns=const.MAIN_COLUMNS_DOUBLE)
-            df_report = pd.concat([df_report, df_to_add])
+                report_data = [
+                    order_dict['Workorder no'],
+                    order_dict['Service No'],
+                    order_dict['Product Code'],
+                    order_dict['Product Description'],
+                    order_dict['Customer Name'],
+                    order_dict['Order Type'],
+                    order_dict['CRD'],
+                    order_dict['Order Taken Date'],
+                    group_1_dict['Group ID'],
+                    group_1_dict['Activity Name'],
+                    group_1_dict['DUE'],
+                    # set RDY date to COM date if RDY date is empty
+                    group_1_dict['RDY'] if not pd.isna(
+                        group_1_dict['RDY']) else group_1_dict['COM'],
+                    # set EXC date to COM date if EXC date is empty
+                    group_1_dict['EXC'] if not pd.isna(
+                        group_1_dict['EXC']) else group_1_dict['COM'],
+                    group_1_dict['DLY'],
+                    group_1_dict['COM'],
+                    group_2_dict['Group ID'],
+                    group_2_dict['Activity Name'],
+                    group_2_dict['DUE'],
+                    # set RDY date to COM date if RDY date is empty
+                    group_2_dict['RDY'] if not pd.isna(
+                        group_2_dict['RDY']) else group_2_dict['COM'],
+                    # set EXC date to COM date if EXC date is empty
+                    group_2_dict['EXC'] if not pd.isna(
+                        group_2_dict['EXC']) else group_2_dict['COM'],
+                    group_2_dict['DLY'],
+                    group_2_dict['COM']
+                ]
+
+                df_to_add = pd.DataFrame(
+                    data=[report_data], columns=const.MAIN_COLUMNS_DOUBLE)
+                df_report = pd.concat([df_report, df_to_add])
 
         return df_report
 
