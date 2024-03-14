4bb5a13da5396c5e08252446894a61d5fd9beee0
Aljo Ponce <aljo.ponce@singtel.com>
2024-03-14 14:41:45 +0800

    major update

--- scripts/reports/pm_project_tracker/helper.py
+++ scripts/reports/pm_project_tracker/helper.py
@@ -12,26 +12,19 @@ def generate_report():
 
     report = OrionReport('Project Tracker Test Report')
     group_name = report.default_config.get("group_name")
-
     report.set_filename(
         f'project_tracker_test_report_{group_name}')
-
     if group_name:
         ProjectTrackerTestGroup = create_project_tracker_test_group_class(
             group_name)
     else:
         ProjectTrackerTestGroup = create_project_tracker_test_group_class()
-
-    # User o2ptest database
-    db = report.test_db
-    # Create table if not exist and insert the service numbers from the file
-    table_model = ProjectTrackerTestGroup
-    db.create_table_from_metadata(table_model)
-
-    # query = report.get_query_from_file("query.sql")
-    # formatted_query = query.format(
-    #     start_date=report.start_date, end_date=report.end_date)
-    # csv_file = report.query_to_csv(formatted_query, add_timestamp=True)
-    # zip_file = report.add_to_zip_file(csv_file, add_timestamp=True)
-    # report.attach_file_to_email(zip_file)
-    # report.send_email()
+    # query to dataframe
+    query = report.get_query_from_file("query_mapping.sql")
+    formatted_query = query.format(group_name=group_name)
+    df = report.query_to_dataframe(
+        formatted_query, query_description="mapping records")
+    # insert records to DB
+    report.insert_df_to_test_table(
+        df, table_name=ProjectTrackerTestGroup.__tablename__, table_model=ProjectTrackerTestGroup, if_exist="replace")
+    excel_file = report.create_excel_from_df(df)
--- scripts/reports/pm_project_tracker/sql/query_mapping.sql
+++ scripts/reports/pm_project_tracker/sql/query_mapping.sql
@@ -1,8 +1,3 @@
-USE o2puat;
-
-DROP TABLE IF EXISTS o2ptest.project_tracker_group;
-
-CREATE TABLE IF NOT EXISTS o2ptest.project_tracker_group AS
 SELECT DISTINCT
     MAP_PRJTRK.project_code,
     MAP_PRJTRK.project_tracker_site_id,
@@ -55,20 +50,20 @@ SELECT DISTINCT
     (
         CASE
             WHEN MAP_PRJTRK.type_of_work = "Diginet"
-            AND MAP_PRJTRK.service_number REGEXP "^M\\d{7}$" THEN "MetroE"
+            AND MAP_PRJTRK.service_number REGEXP "^M\\d{{7}}$" THEN "MetroE"
             WHEN MAP_PRJTRK.type_of_work LIKE "GW%" THEN "Gigawave"
             WHEN MAP_PRJTRK.type_of_work = "FTTH" THEN "Elite"
             WHEN MAP_PRJTRK.type_of_work = "MegaPOP VPN" THEN "MegaPOP"
             WHEN MAP_PRJTRK.type_of_work = "MegaPOP (CE)" THEN "MegaPOP_OR_Ethernet"
             WHEN MAP_PRJTRK.type_of_work = "MegaPOP" THEN "MegaPOP"
             WHEN MAP_PRJTRK.type_of_work = "SingNet"
-            AND MAP_PRJTRK.service_number REGEXP "^ETHCE\\d{8}SNG$" THEN "SingNet_OR_Ethernet"
+            AND MAP_PRJTRK.service_number REGEXP "^ETHCE\\d{{8}}SNG$" THEN "SingNet_OR_Ethernet"
             WHEN MAP_PRJTRK.type_of_work = "SingNet"
-            AND MAP_PRJTRK.service_number REGEXP "^ELITE\\d{8}SNG$" THEN "SingNet"
+            AND MAP_PRJTRK.service_number REGEXP "^ELITE\\d{{8}}SNG$" THEN "SingNet"
             WHEN MAP_PRJTRK.type_of_work = "SingNet"
-            AND MAP_PRJTRK.service_number REGEXP "^GWLGW\\d{7}$" THEN "SingNet"
+            AND MAP_PRJTRK.service_number REGEXP "^GWLGW\\d{{7}}$" THEN "SingNet"
             WHEN MAP_PRJTRK.type_of_work = "Diginet"
-            AND MAP_PRJTRK.service_number REGEXP "^\\d{7}$" THEN "ISDN"
+            AND MAP_PRJTRK.service_number REGEXP "^\\d{{7}}$" THEN "ISDN"
             WHEN MAP_PRJTRK.type_of_work = "Telephone Numbers"
             AND MAP_PRJTRK.service_type = "ISDN" THEN "ISDN_Telephone_Numbers"
             WHEN MAP_PRJTRK.type_of_work = "Cplus VPN" THEN "CPlusIP"
@@ -97,42 +92,42 @@ FROM (
         SELECT DISTINCT
             MAP_PRJ.project_code, MAP_ORD.project_tracker_site_id, MAP_ORD.project_tracker_group_name, MAP_ORD.service_order_number, (
                 CASE
-                    WHEN MAP_ORD.service_number REGEXP "^M\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^M\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "Diginet" THEN "Diginet"
-                    WHEN MAP_ORD.service_number REGEXP "^GW\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^GW\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "Diginet"
                     AND MAP_ORD.product_description LIKE "%Channel%" THEN "GW Channel"
-                    WHEN MAP_ORD.service_number REGEXP "^GW\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^GW\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "Diginet"
                     AND MAP_ORD.product_description LIKE "%Fibre%" THEN "GW Fiber Std"
-                    WHEN MAP_ORD.service_number REGEXP "^GW\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^GW\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "Diginet"
                     AND MAP_ORD.product_description LIKE "%Diversity%" THEN "GW Fiber Diverse"
-                    WHEN MAP_ORD.service_number REGEXP "^GWM\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^GWM\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "Diginet"
                     AND MAP_ORD.product_description = "GWL Monitoring over MetroE" THEN "GW Monitoring"
                     WHEN MAP_ORD.arbor_disp = "TS - FTTH" THEN "FTTH"
                     WHEN MAP_ORD.arbor_disp = "CN - Meg@pop Suite Of IP Services"
                     AND MAP_ORD.product_description = "Intranet VPN" THEN "MegaPOP VPN"
-                    WHEN MAP_ORD.service_number REGEXP "^CE\\d{8}SNG$"
+                    WHEN MAP_ORD.service_number REGEXP "^CE\\d{{8}}SNG$"
                     AND MAP_ORD.arbor_disp = "CN - Meg@pop Suite Of IP Services" THEN "MegaPOP (CE)"
-                    WHEN MAP_ORD.service_number REGEXP "^\\d{8}SNG$"
+                    WHEN MAP_ORD.service_number REGEXP "^\\d{{8}}SNG$"
                     AND MAP_ORD.arbor_disp = "CN - Meg@pop Suite Of IP Services" THEN "MegaPOP"
-                    WHEN MAP_ORD.service_number REGEXP "^ETHCE\\d{8}SNG$"
+                    WHEN MAP_ORD.service_number REGEXP "^ETHCE\\d{{8}}SNG$"
                     AND MAP_ORD.arbor_disp = "SingNet" THEN "SingNet"
-                    WHEN MAP_ORD.service_number REGEXP "^ELITE\\d{8}SNG$"
+                    WHEN MAP_ORD.service_number REGEXP "^ELITE\\d{{8}}SNG$"
                     AND MAP_ORD.arbor_disp = "SingNet" THEN "SingNet"
-                    WHEN MAP_ORD.service_number REGEXP "^GWLGW\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^GWLGW\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "SingNet" THEN "SingNet"
-                    WHEN MAP_ORD.service_number REGEXP "^\\d{7}$"
+                    WHEN MAP_ORD.service_number REGEXP "^\\d{{7}}$"
                     AND MAP_ORD.arbor_disp = "Diginet"
                     AND MAP_ORD.product_description LIKE "%ISDN%" THEN "Diginet"
                     WHEN MAP_ORD.arbor_disp = "ISDN" THEN "Telephone Numbers"
                     WHEN MAP_ORD.arbor_disp = "CN - ConnectPlus IP VPN"
                     AND MAP_ORD.product_description = "Intranet VPN" THEN "Cplus VPN"
-                    WHEN MAP_ORD.service_number REGEXP "^00\\d{6}\\p{L}{3}$"
+                    WHEN MAP_ORD.service_number REGEXP "^00\\d{{6}}\\p{{L}}{{3}}$"
                     AND MAP_ORD.arbor_disp = "CN - ConnectPlus IP VPN" THEN "Cplus PE Port"
-                    WHEN MAP_ORD.service_number REGEXP "^\\p{L}{3}\\d{10}LLC$"
+                    WHEN MAP_ORD.service_number REGEXP "^\\p{{L}}{{3}}\\d{{10}}LLC$"
                     AND MAP_ORD.arbor_disp = "Resale of Overseas Data Svcs" THEN "OLLC (resale data service)"
                     WHEN MAP_ORD.arbor_disp IN ("ILC", "ConnectPlus E-Line") THEN "Cplus ILC / Eline"
                     WHEN MAP_ORD.arbor_disp = "STIX Gateway" THEN "STiX"
@@ -150,17 +145,18 @@ FROM (
                 END
             ) AS type_of_work, MAP_ORD.order_code, MAP_ORD.service_number, MAP_ORD.arbor_disp AS service_type, MAP_ORD.product_description AS order_product_description, MAP_NPP.network_product_desc AS npp_product_description, MAP_PRJ.id AS project_id, MAP_ORD.id AS order_id, MAP_NPP.npp_id, MAP_NPP.product_id
         FROM
-            o2puat.RestInterface_project MAP_PRJ
-            JOIN o2puat.RestInterface_order MAP_ORD ON MAP_ORD.project_id = MAP_PRJ.id
+            RestInterface_project MAP_PRJ
+            JOIN RestInterface_order MAP_ORD ON MAP_ORD.project_id = MAP_PRJ.id
             LEFT JOIN (
                 SELECT NPP.order_id, NPP.id AS npp_id, PRD.id AS product_id, PRD.network_product_desc
-                FROM o2puat.RestInterface_npp NPP
-                    JOIN o2puat.RestInterface_product PRD ON PRD.id = NPP.product_id
+                FROM
+                    RestInterface_npp NPP
+                    JOIN RestInterface_product PRD ON PRD.id = NPP.product_id
                     AND NPP.level = "Mainline"
                     AND NPP.status != "Cancel"
             ) MAP_NPP ON MAP_NPP.order_id = MAP_ORD.id
         WHERE
-            MAP_ORD.project_tracker_group_name = "ZJK8529"
+            MAP_ORD.project_tracker_group_name = '{group_name}'
     ) MAP_PRJTRK
 ORDER BY
     MAP_PRJTRK.project_code,
